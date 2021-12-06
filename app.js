0c3dc5bb8150f501a77ac160a77cfd048afa4864
Jan Daniel Fermin <jandanielfermin@gmail.com>
2021-12-05 18:41:41 -0800

    added basic login

--- app.js
+++ app.js
@@ -4,12 +4,23 @@ const db = require('./config/keys').mongoURI;
 const users = require('./routes/api/users');
 const tweets = require('./routes/api/tweets');
 const app = express();
+const User = require('./models/User')
+const bodyParser = require('body-parser')
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    
+    res.send("Hello World")
+});
 
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
